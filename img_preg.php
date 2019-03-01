214853dfc7bf8b3e64ed4f8da5e9758fefc053e4
yangqinjiang <531553280@qq.com>
2019-03-01 23:29:11 +0800

    请匹配所有img标签中的src值

--- img_preg.php
+++ img_preg.php
@@ -1,6 +1,6 @@
 <?php
 // 请匹配所有img标签中的src值
-$str = '<img alt="高清无码" id="av" src="av.jpg" />';
+$str = '<img alt="高清无码" id="av" src="av.jpg" /><img alt="高清无码" id="bv" src="bv.jpg" />';
 $pattern = '/<img.*?src="(.*?)".*?\/?>/i'; // /忽略大小写
-preg_match($pattern,$str,$match);
+preg_match_all($pattern,$str,$match);
 var_dump($match);
